f39a866f4a77c8dd41e9e1da6b6fc1f86dfe46de
Michał Styrylski <styrylski.michal@gmail.com>
2018-05-01 23:09:27 +0200

    nie wiem co się stało

--- static/js/Trial.js
+++
@@ -1,5 +0,0 @@
-function Trial(){
-  var trialTexture = THREE.ImageUtils.loadTexture("mats/trial.png");
-  var trialMaterial = new THREE.SpriteMaterial( { map: trialTexture, useScreenCoordinates: true} );
-	this.sprite = new THREE.Sprite( trialMaterial );
-}
