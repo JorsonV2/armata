416a3a3a2629e403ffe1a69e01333299be158a07
Michał Styrylski <styrylski.michal@gmail.com>
2018-05-22 08:59:27 +0200

    move

--- static/js/Net.js
+++ static/js/Net.js
@@ -5,13 +5,17 @@ function Net() {
     client = io();
     client.emit("new_user", $('#tb_login').val(), function(data) {});
 
-    client.on("movePlayer", function(data) {
-    //  console.log(data);
-      //  game.movePlayer(data);
+    client.on("mp", function(data) {
+      //console.log(data);
+      game.movePlayer(data);
     })
 
-    client.on("movePlayer2", function(data) {
-      game.movePlayer(data);
+    client.on("rp", function(data) {
+      game.rotatePlayer(data);
+    })
+
+    client.on("sp", function(data) {
+      game.shotPlayer(data);
     })
 
     client.on("remove", function(data) {
@@ -25,15 +29,15 @@ function Net() {
   }
 
   this.movePlayer = function(move) {
-    client.emit("movePlayer", move)
+    client.emit("mp", move)
   }
 
   this.rotatePlayer = function(move) {
-    client.emit("rotatePlayer", move)
+    client.emit("rp", move)
   }
 
   this.shotPlayer = function(move) {
-    client.emit("shotPlayer", move)
+    client.emit("sp", move)
   }
 
   this.id = function() {
